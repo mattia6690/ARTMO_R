3892a104d4660dbd819c30f954b1e74eb5a534d5
Mattia <mattia.rossi@eurac.edu>
2018-09-25 16:45:53 +0200

    Added essential packages and functions

---
+++ R/Essentials.R
@@ -0,0 +1,25 @@
+
+
+# Required Packages -------------------------------------------------------
+library("devtools")
+install_github("mattia6690/Mfunctions");library(Mfunctions)
+loadandinstall("raster")
+loadandinstall("sf")
+loadandinstall("tools")
+loadandinstall("tidyverse")
+
+
+# Functions ---------------------------------------------------------------
+
+# Transforms an lm function output to text usable for ggplots
+# Greetings to Jodie Burchell (http://t-redactyl.io/)
+r2.equation = function(x) {
+  lm_coef <- list(a = round(coef(x)[1], digits = 2),
+                  b = round(coef(x)[2], digits = 2),
+                  r2 = round(summary(x)$r.squared, digits = 2));
+  lm_eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,lm_coef)
+  as.character(as.expression(lm_eq))
+}
+
+
+
