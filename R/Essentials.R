4d820f6f848af314e90a3560b3950767d35a8745
Mattia <mattia.rossi@eurac.edu>
2018-09-25 17:46:24 +0200

    added lubridate and numeric coefficients

--- R/Essentials.R
+++ R/Essentials.R
@@ -7,6 +7,7 @@ loadandinstall("raster")
 loadandinstall("sf")
 loadandinstall("tools")
 loadandinstall("tidyverse")
+loadandinstall("lubridate")
 
 
 # Functions ---------------------------------------------------------------
@@ -14,8 +15,8 @@ loadandinstall("tidyverse")
 # Transforms an lm function output to text usable for ggplots
 # Greetings to Jodie Burchell (http://t-redactyl.io/)
 r2.equation = function(x) {
-  lm_coef <- list(a = round(coef(x)[1], digits = 2),
-                  b = round(coef(x)[2], digits = 2),
+  lm_coef <- list(a = round(as.numeric(coef(x)[1]), digits = 2),
+                  b = round(as.numeric(coef(x)[2]), digits = 2),
                   r2 = round(summary(x)$r.squared, digits = 2));
   lm_eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,lm_coef)
   as.character(as.expression(lm_eq))
