5c49773343222637143974a4f16f48568f006984
Mattia <mattia.rossi@eurac.edu>
2018-09-28 16:22:09 +0200

    artmo table spectral and bio as parameter

--- R/ARTMO_ValidationTables.R
+++ R/ARTMO_ValidationTables.R
@@ -8,20 +8,22 @@ outdir <- "K:/SentinelVegetationProducts/S2_LAI"
 suffix <- "270918"
 
 wave  <-read.csv(paste0(outdir,"/Other/Sentinel2_bands.csv"))
-wave.c<- wave %>% filter(Resolution!=60) %>% dplyr::select(Central) %>% c(0,.) %>% unlist(use.names = F)
+wave.c<- wave %>% filter(Resolution!=60) %>% dplyr::select(Central) %>% unlist(use.names = F)
 
 import<-readRDS(paste0(outdir,"/R_Objects/ExtractionList_",suffix,".rds"))
+input.cols<-c("LAI","MTA","Sunangle")
+spectral.col<-"S.Band_"
 
+wave.artmo<-c(rep(0,times=length(input.cols)),wave.c)
 # ARTMO validation table ----------------------------------------------------
 artmo1<-import %>% 
-  as.tibble %>% 
-  dplyr::select(.,Station,Date,OP1,meanLAI,contains("Band_")) %>% 
+  dplyr::select(.,Station,Date,OP1,one_of(input.cols),contains(spectral.col)) %>% 
   group_by(Station,Date,OP1) %>% 
   nest
 
 artmo2<- map(artmo1$data,function(x){x %>% unlist %>% as.numeric() %>% t %>% t})
 artmo3<- do.call(cbind,artmo2)
-artmo4<- cbind(wave.c,artmo3)
+artmo4<- cbind(wave.artmo,artmo3)
 
 write.table(artmo4,file = paste0(outdir,"/Validation_Tables/Metrics_S2_Artmo_10Bands_",suffix,".csv"),col.names = FALSE,row.names = F,sep=",")
 
