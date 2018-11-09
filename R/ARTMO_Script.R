5f288e5687935475439be2e78875f6ceaff86926
mattia6690 <mattia.rossi@eurac.edu>
2018-11-09 14:44:29 +0100

    Added Shiny call to main

--- R/ARTMO_Script.R
+++ R/ARTMO_Script.R
@@ -4,6 +4,9 @@
 # Environment -------------------------------
 source("R/Essentials.R")
 source("R/MySQL_Functions.R")
+source("R/COST_functions.R")
+source("R/COST_tables.R")
+
 
 database  <-"artmo_1"
 directory <- "K:/SentinelVegetationProducts/S2_LAI/Databases/"
@@ -13,3 +16,12 @@ user="root";host="localhost";pw="123456"
 con <- connect.raw(user=user, password= pw, host=host)
 con <- connect.db (user=user, password= pw, database=database, host=host)
 
+costs<-get.cost.tabs(con)
+
+
+
+
+# Run the Shiny WebApp
+source("R/ui.R")
+source("R/server.R")
+shinyApp(ui=ui,server=server)
