bbdc4788f7786209c994086e6fe6b44555539f3b
mattia6690 <mattia.rossi@eurac.edu>
2018-11-19 18:08:31 +0100

    MLA Functions Exported

---
+++ R/Fun_MLA.R
@@ -0,0 +1,39 @@
+#' Functions for Extracting the MLA Results
+#' 
+#' 
+################################'
+
+#### NOTE: Spelling mistakes are in the MySQL Database. No Errors
+
+# Plotcv Table
+.mlr.plotcv<-function(jtable){
+  
+  tabout<-jtable %>% 
+    mutate(plotcv=map(plotcv,function(x) {
+      
+      mat<-readMat(x)
+      mat.melt<- mat$data[,,1] %>% melt %>% select(ID=Var1,L1,value)
+      mat.spread<- mat.melt %>% spread(L1,value)
+    }))
+  
+  return(tabout)
+  
+}
+
+# Statisticscv Table
+.mlr.staticscv<-function(jtable){
+  
+  tabout<-jtable %>% 
+    mutate(staticscv=map(staticscv,function(x) {readMat(x) %>% .$data %>% as.numeric}))
+  return(tabout)
+  
+}
+
+# WL Table
+.mlr.wl<-function(jtable){
+  
+  tabout<-jtable %>% 
+    mutate(wl=map(wl,function(x) as.numeric(rawTrans(x)$numbers)))
+  return(tabout)
+  
+}
